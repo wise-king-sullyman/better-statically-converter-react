ef2e9d4bf17ab330a695fb6cedba23b430aedb8b
Sully <wise.king.sullyman@gmail.com>
2022-09-17 18:33:46 -0400

    Add clipboard copy functionality

--- src/components/LinkDisplay.tsx
+++ src/components/LinkDisplay.tsx
@@ -1,12 +1,49 @@
-import React from "react";
-import { Menu, MenuContent, MenuList, MenuItem } from "@patternfly/react-core";
+import React, { useState } from "react";
+import {
+  Menu,
+  MenuContent,
+  MenuList,
+  MenuItem,
+  Tooltip,
+} from "@patternfly/react-core";
+import { CopyIcon } from "@patternfly/react-icons";
 
 export interface LinkDisplayProps {
   links?: string[];
 }
 
 export function LinkDisplay({ links }: LinkDisplayProps) {
-  const menuItems = links?.map((link) => <MenuItem key={link}>{link}</MenuItem>);
+  const [selectedLink, setSelectedLink] = useState("");
+
+  function copyToClipboard(link: string) {
+    if (!navigator.clipboard) {
+      console.log("Clipboard API not available");
+      return;
+    }
+
+    try {
+      navigator.clipboard.writeText(link);
+      setSelectedLink(link);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const menuItems = links?.map((link) => {
+    const linkCopyStatus = link === selectedLink ? "Copied" : "Click to copy";
+
+    return (
+      <Tooltip content={linkCopyStatus} position="left">
+        <MenuItem
+          key={link}
+          icon={<CopyIcon />}
+          onClick={() => copyToClipboard(link)}
+        >
+          {link}
+        </MenuItem>
+      </Tooltip>
+    );
+  });
 
   return (
     <Menu>
